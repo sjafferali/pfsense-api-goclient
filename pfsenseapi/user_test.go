3a6bed93cb7996819a62b4d900cc9b6fdc9eda72
Samir Jafferali <me@samir.id>
2023-10-17 14:47:47 -0700

    Add more user tests

--- pfsenseapi/user_test.go
+++ pfsenseapi/user_test.go
@@ -44,6 +44,56 @@ func TestUserService_ListUsers(t *testing.T) {
 	require.Len(t, response, 2)
 }
 
+func TestUserService_CreateUser(t *testing.T) {
+	data := mustReadFileString(t, "testdata/createuser.json")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = io.WriteString(w, data)
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+
+	newClient := NewClientWithNoAuth(server.URL)
+	response, err := newClient.User.CreateUser(context.Background(), UserRequest{})
+	require.NotNil(t, response)
+	require.NoError(t, err)
+}
+
+func TestUserService_UpdateUser(t *testing.T) {
+	data := mustReadFileString(t, "testdata/createuser.json")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = io.WriteString(w, data)
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+
+	newClient := NewClientWithNoAuth(server.URL)
+	response, err := newClient.User.UpdateUser(context.Background(), UserRequest{})
+	require.NotNil(t, response)
+	require.NoError(t, err)
+}
+
+func TestUserService_DeleteUser(t *testing.T) {
+	data := mustReadFileString(t, "testdata/createuser.json")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = io.WriteString(w, data)
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+
+	newClient := NewClientWithNoAuth(server.URL)
+	err := newClient.User.DeleteUser(context.Background(), "testing123")
+	require.NoError(t, err)
+}
+
 func TestUserService_CreateGroup(t *testing.T) {
 	data := mustReadFileString(t, "testdata/creategroup.json")
 
@@ -75,5 +125,21 @@ func TestUserService_UpdateGroup(t *testing.T) {
 	newClient := NewClientWithNoAuth(server.URL)
 	response, err := newClient.User.UpdateGroup(context.Background(), "admin", GroupRequest{})
 	require.NotNil(t, response)
+	require.NoError(t, err)
+}
+
+func TestUserService_DeleteGroup(t *testing.T) {
+	data := mustReadFileString(t, "testdata/creategroup.json")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = io.WriteString(w, data)
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+
+	newClient := NewClientWithNoAuth(server.URL)
+	err := newClient.User.DeleteGroup(context.Background(), "admin")
 	require.NoError(t, err)
 }
